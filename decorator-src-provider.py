c5271c30b546ad511b7d9e94ab1d4da5e6d1542e
langsehe27702marcriddle <lyubomira-s-12552915@rambler.ru>
2025-12-04 18:22:29 +0800

    add template.rst, index.ts, decorator-src-provider.py

---
+++ decorator-src-provider.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\decorator-src-provider.py
+// Generated at: 2025-10-13T04:17:06.877Z
---
+++ template.rst
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\template.rst
+// Generated at: 2025-10-13T04:17:06.876Z
